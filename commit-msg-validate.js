08697efb5b87b249dee454bacf8589d20eca56b4
Jeffrey Priebe <jeffreypriebe@gmail.com>
2018-02-08 11:51:17 -0800

    Feat: Add override option

--- commit-msg-validate.js
+++ commit-msg-validate.js
@@ -9,6 +9,13 @@ const data = fs.readFileSync(filename, 'utf8');
 
 if (!data) return console.log(3);
 
+// allow override of rules for the occasional use
+if (data.startsWith('!!')) {
+    const newData = data.substring(2);
+    fs.writeFileSync(filename, newData);
+    return console.log(0);
+}
+
 const valid = validateMessage(data, filename);
 
 console.log(valid ? 0 : 1);
